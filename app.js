43aa61511fa3ad3f76802e532ded0228becefc55
Webstar Maseko <webstarsiyabonga@gmail.com>
2020-10-29 12:24:26 +0200

    added a procfile and server

--- app.js
+++ app.js
@@ -21,6 +21,6 @@ app.get("/contact", function(req,res){
     res.render("contact");
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("listening on 3000");
 });
