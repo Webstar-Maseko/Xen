a7022f4012035b2b689f84f43655122b96526904
Webstar Maseko <webstarsiyabonga@gmail.com>
2020-10-29 11:59:28 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+//jshint esversion:6
+
+const exp = require('express');
+
+const app = exp();
+app.use(exp.static("static"));
+app.set("view engine", "ejs");
+
+
+
+app.get("/", function(req,res){
+  res.render("index");
+});
+app.get("/about", function(req,res){
+    res.render("about");
+});
+app.get("/services", function(req,res){
+    res.render("services");
+});
+app.get("/contact", function(req,res){
+    res.render("contact");
+});
+
+app.listen(3000, function(){
+  console.log("listening on 3000");
+});
